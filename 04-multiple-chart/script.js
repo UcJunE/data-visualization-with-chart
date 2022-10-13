9fe582e5634e190cb502441a24e7962b7b832334
JunE <ucjune90@gmail.com>
2022-10-14 02:47:50 +0800

    multiple chart

--- 04-multiple-chart/script.js
+++ 04-multiple-chart/script.js
@@ -1,16 +1,66 @@
-const options = {
+const sightings = [10, 13, 15, 22, 34, 23, 55, 78, 44, 31];
+const temperature = [33, 21, 22, 24, 25, 26, 26, 21, 31, 44];
+const months = [
+  "Jan",
+  "Feb",
+  "Mar",
+  "Apr",
+  "May",
+  "Jun",
+  "Jul",
+  "Aug",
+  "Sept",
+  "Oct",
+];
+
+const lineChartOptions = {
   chart: {
-    type: "pie",
+    type: "line",
     height: "100%",
   },
-  // each series represents one set of data
-  series: [21, 23, 22, 27, 45],
-
-  // what is are the labels along the x-axis (horizontal line)
-  labels: ["English", "Mathematics", "Mother Tongue", "Science", "PE"],
-  
+  series: [
+    {
+      name: "sightings",
+      data: sightings,
+    },
+    {
+      name: "temperature",
+      data: temperature,
+    },
+  ],
+  xaxis: {
+    category: months,
+  },
 };
 
-let chart = new ApexCharts(document.querySelector("#chart"), options);
+const lineChart = new ApexCharts(
+  document.querySelector("#line-chart"),
+  lineChartOptions
+);
+lineChart.render();
 
-chart.render();
+const barChartOptions = {
+  chart: {
+    type: "bar",
+    height: "100%",
+  },
+  series: [
+    {
+      name: "sightings",
+      data: sightings,
+    },
+    {
+      name: "temperature",
+      data: temperature,
+    },
+  ],
+  xaxis: {
+    category: months,
+  },
+};
+
+const barChart = new ApexCharts(
+  document.querySelector("#bar-chart"),
+  barChartOptions
+);
+barChart.render();
